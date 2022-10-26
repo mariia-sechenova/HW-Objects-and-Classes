0c4bf31d9f4e9b2e2358477d732915f9cd4edead
Mariia Sechenova <sechenova.m.a@yandex.ru>
2022-10-26 22:14:15 +0700

    The HW is finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book kgbt = new Book(2022, "Pelevin");
+        Book goneWithTheWind = new Book(1939, "Mitchell");
+
+
+        kgbt.setYear();
+        int kgbtYear = kgbt.getYear();
+        System.out.println(kgbtYear);
     }
+
 }
